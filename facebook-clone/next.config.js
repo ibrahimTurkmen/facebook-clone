eb90debaaac7fbe87cdf144c6f432edbd6194ef5
İbrahim Türkmen <ie.turkmen@gmail.com>
2022-06-17 23:22:25 +0300

    fixed

--- facebook-clone/next.config.js
+++ facebook-clone/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    domains: ['upload.wikimedia.org','platform-lookaside.fbsbx.com']
+  }
 }
 
 module.exports = nextConfig
--- my-app/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images:{
-    domains: ['upload.wikimedia.org','platform-lookaside.fbsbx.com']
-  }
-}
-
-module.exports = nextConfig
